af01378e4def21b3ac934a2b63edcac9ba12f23f
MinhThang <dominhtoan9467@gmail.com>
2025-06-27 18:50:51 +0700

    fix bug file lon tam thoi

---
+++ HomeForm.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace Menu_Management
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeForm));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            label1 = new Label();
+            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel1 = new Panel();
+            guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            label2 = new Label();
+            panel2 = new Panel();
+            guna2CirclePictureBox2 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            label3 = new Label();
+            panel3 = new Panel();
+            guna2CirclePictureBox3 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            label4 = new Label();
+            panel4 = new Panel();
+            guna2CirclePictureBox4 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            label5 = new Label();
+            panel5 = new Panel();
+            guna2CirclePictureBox5 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            label6 = new Label();
+            panel6 = new Panel();
+            label7 = new Label();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            panel7 = new Panel();
+            flowLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox2).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox3).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox4).BeginInit();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox5).BeginInit();
+            panel6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(82, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Dishes";
+            // 
+            // guna2TextBox1
+            // 
+            guna2TextBox1.BorderRadius = 20;
+            guna2TextBox1.BorderThickness = 0;
+            guna2TextBox1.CustomizableEdges = customizableEdges1;
+            guna2TextBox1.DefaultText = "";
+            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.FillColor = Color.FromArgb(224, 224, 224);
+            guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2TextBox1.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.IconRight = (Image)resources.GetObject("guna2TextBox1.IconRight");
+            guna2TextBox1.IconRightSize = new Size(36, 36);
+            guna2TextBox1.Location = new Point(266, 32);
+            guna2TextBox1.Margin = new Padding(4);
+            guna2TextBox1.Name = "guna2TextBox1";
+            guna2TextBox1.PlaceholderForeColor = Color.Silver;
+            guna2TextBox1.PlaceholderText = "Search dish";
+            guna2TextBox1.SelectedText = "";
+            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2TextBox1.Size = new Size(385, 45);
+            guna2TextBox1.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.Controls.Add(panel1);
+            flowLayoutPanel1.Controls.Add(panel2);
+            flowLayoutPanel1.Controls.Add(panel3);
+            flowLayoutPanel1.Controls.Add(panel4);
+            flowLayoutPanel1.Controls.Add(panel5);
+            flowLayoutPanel1.Location = new Point(82, 99);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(651, 179);
+            flowLayoutPanel1.TabIndex = 2;
+            flowLayoutPanel1.WrapContents = false;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(guna2CirclePictureBox1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(10, 10);
+            panel1.Margin = new Padding(10, 10, 0, 10);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(180, 140);
+            panel1.TabIndex = 3;
+            // 
+            // guna2CirclePictureBox1
+            // 
+            guna2CirclePictureBox1.Image = (Image)resources.GetObject("guna2CirclePictureBox1.Image");
+            guna2CirclePictureBox1.ImageRotate = 0F;
+            guna2CirclePictureBox1.Location = new Point(41, 3);
+            guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            guna2CirclePictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges3;
+            guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox1.Size = new Size(105, 102);
+            guna2CirclePictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            guna2CirclePictureBox1.TabIndex = 4;
+            guna2CirclePictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(41, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Breakfast";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(guna2CirclePictureBox2);
+            panel2.Controls.Add(label3);
+            panel2.Location = new Point(200, 10);
+            panel2.Margin = new Padding(10, 10, 0, 10);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(180, 140);
+            panel2.TabIndex = 5;
+            // 
+            // guna2CirclePictureBox2
+            // 
+            guna2CirclePictureBox2.Image = (Image)resources.GetObject("guna2CirclePictureBox2.Image");
+            guna2CirclePictureBox2.ImageRotate = 0F;
+            guna2CirclePictureBox2.Location = new Point(41, 3);
+            guna2CirclePictureBox2.Name = "guna2CirclePictureBox2";
+            guna2CirclePictureBox2.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2CirclePictureBox2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox2.Size = new Size(105, 102);
+            guna2CirclePictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            guna2CirclePictureBox2.TabIndex = 4;
+            guna2CirclePictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(59, 108);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 30);
+            label3.TabIndex = 1;
+            label3.Text = "Drinks";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(guna2CirclePictureBox3);
+            panel3.Controls.Add(label4);
+            panel3.Location = new Point(390, 10);
+            panel3.Margin = new Padding(10, 10, 0, 10);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(180, 140);
+            panel3.TabIndex = 5;
+            // 
+            // guna2CirclePictureBox3
+            // 
+            guna2CirclePictureBox3.Image = (Image)resources.GetObject("guna2CirclePictureBox3.Image");
+            guna2CirclePictureBox3.ImageRotate = 0F;
+            guna2CirclePictureBox3.Location = new Point(41, 3);
+            guna2CirclePictureBox3.Name = "guna2CirclePictureBox3";
+            guna2CirclePictureBox3.ShadowDecoration.CustomizableEdges = customizableEdges5;
+            guna2CirclePictureBox3.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox3.Size = new Size(105, 102);
+            guna2CirclePictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            guna2CirclePictureBox3.TabIndex = 4;
+            guna2CirclePictureBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(56, 108);
+            label4.Name = "label4";
+            label4.Size = new Size(69, 30);
+            label4.TabIndex = 1;
+            label4.Text = "Soups";
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(guna2CirclePictureBox4);
+            panel4.Controls.Add(label5);
+            panel4.Location = new Point(580, 10);
+            panel4.Margin = new Padding(10, 10, 0, 10);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(180, 140);
+            panel4.TabIndex = 5;
+            // 
+            // guna2CirclePictureBox4
+            // 
+            guna2CirclePictureBox4.Image = (Image)resources.GetObject("guna2CirclePictureBox4.Image");
+            guna2CirclePictureBox4.ImageRotate = 0F;
+            guna2CirclePictureBox4.Location = new Point(41, 3);
+            guna2CirclePictureBox4.Name = "guna2CirclePictureBox4";
+            guna2CirclePictureBox4.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2CirclePictureBox4.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox4.Size = new Size(105, 102);
+            guna2CirclePictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            guna2CirclePictureBox4.TabIndex = 4;
+            guna2CirclePictureBox4.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(56, 108);
+            label5.Name = "label5";
+            label5.Size = new Size(76, 30);
+            label5.TabIndex = 1;
+            label5.Text = "Snacks";
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(guna2CirclePictureBox5);
+            panel5.Controls.Add(label6);
+            panel5.Location = new Point(770, 10);
+            panel5.Margin = new Padding(10, 10, 0, 10);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(180, 140);
+            panel5.TabIndex = 5;
+            // 
+            // guna2CirclePictureBox5
+            // 
+            guna2CirclePictureBox5.Image = (Image)resources.GetObject("guna2CirclePictureBox5.Image");
+            guna2CirclePictureBox5.ImageRotate = 0F;
+            guna2CirclePictureBox5.Location = new Point(41, 3);
+            guna2CirclePictureBox5.Name = "guna2CirclePictureBox5";
+            guna2CirclePictureBox5.ShadowDecoration.CustomizableEdges = customizableEdges7;
+            guna2CirclePictureBox5.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox5.Size = new Size(105, 102);
+            guna2CirclePictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            guna2CirclePictureBox5.TabIndex = 4;
+            guna2CirclePictureBox5.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(41, 108);
+            label6.Name = "label6";
+            label6.Size = new Size(98, 30);
+            label6.TabIndex = 1;
+            label6.Text = "Breakfast";
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(label7);
+            panel6.Controls.Add(flowLayoutPanel2);
+            panel6.Location = new Point(82, 284);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(651, 480);
+            panel6.TabIndex = 3;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 15.75F);
+            label7.Location = new Point(15, 16);
+            label7.Name = "label7";
+            label7.Size = new Size(98, 30);
+            label7.TabIndex = 1;
+            label7.Text = "Breakfast";
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel2.Location = new Point(0, 59);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(651, 427);
+            flowLayoutPanel2.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            panel7.Dock = DockStyle.Right;
+            panel7.Location = new Point(739, 0);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(372, 764);
+            panel7.TabIndex = 4;
+            // 
+            // HomeForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1111, 764);
+            Controls.Add(panel7);
+            Controls.Add(panel6);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(guna2TextBox1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "HomeForm";
+            Text = "HomeForm";
+            flowLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox2).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox3).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox4).EndInit();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox5).EndInit();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel1;
+        private Label label2;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private Panel panel2;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox2;
+        private Label label3;
+        private Panel panel3;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox3;
+        private Label label4;
+        private Panel panel4;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox4;
+        private Label label5;
+        private Panel panel5;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox5;
+        private Label label6;
+        private Panel panel6;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private Label label7;
+        private Panel panel7;
+    }
+}

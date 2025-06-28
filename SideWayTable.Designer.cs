3f5bd9e76a098e0d31c88936943e18d9571ec592
HungNguyen <Hungnguyentuanlay@gmail.com>
2025-06-28 19:54:07 +0700

    fix xong cai tables

--- SideWayTable.Designer.cs
+++ SideWayTable.Designer.cs
@@ -28,17 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            table = new Guna.UI2.WinForms.Guna2Panel();
+            NumberGuest = new Label();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            ResevDate = new Label();
+            CusName = new Label();
+            TableStatus = new Label();
+            TableLable = new Label();
+            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Panel4 = new Guna.UI2.WinForms.Guna2Panel();
+            table.SuspendLayout();
             SuspendLayout();
+            // 
+            // table
+            // 
+            table.BackColor = SystemColors.Control;
+            table.BorderColor = Color.White;
+            table.BorderRadius = 30;
+            table.BorderThickness = 2;
+            table.Controls.Add(guna2Panel1);
+            table.Controls.Add(NumberGuest);
+            table.Controls.Add(ResevDate);
+            table.Controls.Add(CusName);
+            table.Controls.Add(TableStatus);
+            table.Controls.Add(TableLable);
+            table.CustomizableEdges = customizableEdges13;
+            table.FillColor = Color.LightGray;
+            table.Location = new Point(3, 47);
+            table.Name = "table";
+            table.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            table.Size = new Size(284, 140);
+            table.TabIndex = 10;
+            // 
+            // NumberGuest
+            // 
+            NumberGuest.AutoSize = true;
+            NumberGuest.BackColor = Color.Transparent;
+            NumberGuest.Location = new Point(57, 91);
+            NumberGuest.Name = "NumberGuest";
+            NumberGuest.Size = new Size(98, 15);
+            NumberGuest.TabIndex = 2;
+            NumberGuest.Text = "Number of Guest";
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BorderRadius = 30;
+            customizableEdges11.BottomLeft = false;
+            customizableEdges11.TopLeft = false;
+            guna2Panel1.CustomizableEdges = customizableEdges11;
+            guna2Panel1.FillColor = Color.FromArgb(128, 255, 128);
+            guna2Panel1.Location = new Point(243, 1);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2Panel1.Size = new Size(41, 137);
+            guna2Panel1.TabIndex = 6;
+            // 
+            // ResevDate
+            // 
+            ResevDate.AutoSize = true;
+            ResevDate.BackColor = Color.Transparent;
+            ResevDate.Location = new Point(57, 24);
+            ResevDate.Name = "ResevDate";
+            ResevDate.Size = new Size(107, 15);
+            ResevDate.TabIndex = 2;
+            ResevDate.Text = "Reseve date + time";
+            // 
+            // CusName
+            // 
+            CusName.AutoSize = true;
+            CusName.BackColor = Color.Transparent;
+            CusName.Location = new Point(57, 76);
+            CusName.Name = "CusName";
+            CusName.Size = new Size(94, 15);
+            CusName.TabIndex = 2;
+            CusName.Text = "Customer Name";
+            // 
+            // TableStatus
+            // 
+            TableStatus.AutoSize = true;
+            TableStatus.BackColor = Color.Transparent;
+            TableStatus.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            TableStatus.ForeColor = Color.Lime;
+            TableStatus.Location = new Point(57, 106);
+            TableStatus.Name = "TableStatus";
+            TableStatus.Size = new Size(73, 21);
+            TableStatus.TabIndex = 1;
+            TableStatus.Text = "Available";
+            // 
+            // TableLable
+            // 
+            TableLable.AutoSize = true;
+            TableLable.BackColor = Color.Transparent;
+            TableLable.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            TableLable.Location = new Point(212, 18);
+            TableLable.Name = "TableLable";
+            TableLable.Size = new Size(25, 21);
+            TableLable.TabIndex = 1;
+            TableLable.Text = "T1";
+            // 
+            // guna2Panel2
+            // 
+            guna2Panel2.BackColor = SystemColors.Control;
+            guna2Panel2.BorderColor = Color.White;
+            guna2Panel2.BorderRadius = 25;
+            guna2Panel2.BorderThickness = 2;
+            guna2Panel2.CustomizableEdges = customizableEdges15;
+            guna2Panel2.FillColor = Color.LightGray;
+            guna2Panel2.Location = new Point(32, 6);
+            guna2Panel2.Name = "guna2Panel2";
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            guna2Panel2.Size = new Size(65, 224);
+            guna2Panel2.TabIndex = 11;
+            // 
+            // guna2Panel3
+            // 
+            guna2Panel3.BackColor = SystemColors.Control;
+            guna2Panel3.BorderColor = Color.White;
+            guna2Panel3.BorderRadius = 25;
+            guna2Panel3.BorderThickness = 2;
+            guna2Panel3.CustomizableEdges = customizableEdges17;
+            guna2Panel3.FillColor = Color.LightGray;
+            guna2Panel3.Location = new Point(103, 6);
+            guna2Panel3.Name = "guna2Panel3";
+            guna2Panel3.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            guna2Panel3.Size = new Size(65, 224);
+            guna2Panel3.TabIndex = 11;
+            // 
+            // guna2Panel4
+            // 
+            guna2Panel4.BackColor = SystemColors.Control;
+            guna2Panel4.BorderColor = Color.White;
+            guna2Panel4.BorderRadius = 25;
+            guna2Panel4.BorderThickness = 2;
+            guna2Panel4.CustomizableEdges = customizableEdges19;
+            guna2Panel4.FillColor = Color.LightGray;
+            guna2Panel4.Location = new Point(174, 6);
+            guna2Panel4.Name = "guna2Panel4";
+            guna2Panel4.ShadowDecoration.CustomizableEdges = customizableEdges20;
+            guna2Panel4.Size = new Size(65, 224);
+            guna2Panel4.TabIndex = 11;
             // 
             // SideWayTable
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(table);
+            Controls.Add(guna2Panel4);
+            Controls.Add(guna2Panel3);
+            Controls.Add(guna2Panel2);
             Name = "SideWayTable";
-            Size = new Size(402, 150);
+            Size = new Size(292, 231);
+            table.ResumeLayout(false);
+            table.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel table;
+        private Label NumberGuest;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Label ResevDate;
+        private Label CusName;
+        private Label TableStatus;
+        private Label TableLable;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel4;
     }
 }
--- TableForm.Designer.cs
+++ TableForm.Designer.cs
@@ -28,19 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            longTable1 = new LongTable();
+            longTable2 = new LongTable();
+            sideWayTable1 = new SideWayTable();
+            smallTable1 = new SmallTable();
+            smallTable2 = new SmallTable();
+            smallTable3 = new SmallTable();
+            smallTable4 = new SmallTable();
+            sideWayTable2 = new SideWayTable();
+            smallTable5 = new SmallTable();
+            label1 = new Label();
+            SeachTB = new Guna.UI2.WinForms.Guna2TextBox();
             SuspendLayout();
+            // 
+            // longTable1
+            // 
+            longTable1.Location = new Point(54, 99);
+            longTable1.Name = "longTable1";
+            longTable1.Size = new Size(205, 254);
+            longTable1.TabIndex = 0;
+            // 
+            // longTable2
+            // 
+            longTable2.Location = new Point(553, 522);
+            longTable2.Name = "longTable2";
+            longTable2.Size = new Size(205, 254);
+            longTable2.TabIndex = 0;
+            // 
+            // sideWayTable1
+            // 
+            sideWayTable1.Location = new Point(23, 522);
+            sideWayTable1.Name = "sideWayTable1";
+            sideWayTable1.Size = new Size(292, 231);
+            sideWayTable1.TabIndex = 1;
+            // 
+            // smallTable1
+            // 
+            smallTable1.Location = new Point(54, 359);
+            smallTable1.Name = "smallTable1";
+            smallTable1.Size = new Size(202, 147);
+            smallTable1.TabIndex = 2;
+            // 
+            // smallTable2
+            // 
+            smallTable2.Location = new Point(556, 359);
+            smallTable2.Name = "smallTable2";
+            smallTable2.Size = new Size(202, 147);
+            smallTable2.TabIndex = 2;
+            // 
+            // smallTable3
+            // 
+            smallTable3.Location = new Point(320, 359);
+            smallTable3.Name = "smallTable3";
+            smallTable3.Size = new Size(202, 147);
+            smallTable3.TabIndex = 2;
+            // 
+            // smallTable4
+            // 
+            smallTable4.Location = new Point(333, 568);
+            smallTable4.Name = "smallTable4";
+            smallTable4.Size = new Size(202, 147);
+            smallTable4.TabIndex = 2;
+            // 
+            // sideWayTable2
+            // 
+            sideWayTable2.Location = new Point(465, 99);
+            sideWayTable2.Name = "sideWayTable2";
+            sideWayTable2.Size = new Size(293, 232);
+            sideWayTable2.TabIndex = 3;
+            // 
+            // smallTable5
+            // 
+            smallTable5.Location = new Point(265, 143);
+            smallTable5.Name = "smallTable5";
+            smallTable5.Size = new Size(202, 147);
+            smallTable5.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("VNI-Disney", 24F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(54, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 39);
+            label1.TabIndex = 4;
+            label1.Text = "TABLES";
+            // 
+            // SeachTB
+            // 
+            SeachTB.BackColor = Color.Transparent;
+            SeachTB.BorderRadius = 20;
+            customizableEdges1.TopRight = false;
+            SeachTB.CustomizableEdges = customizableEdges1;
+            SeachTB.DefaultText = "";
+            SeachTB.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            SeachTB.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            SeachTB.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            SeachTB.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            SeachTB.FillColor = Color.FromArgb(255, 192, 128);
+            SeachTB.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            SeachTB.Font = new Font("Segoe UI", 9F);
+            SeachTB.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            SeachTB.Location = new Point(505, 45);
+            SeachTB.Name = "SeachTB";
+            SeachTB.PlaceholderText = "Find Guest";
+            SeachTB.SelectedText = "";
+            SeachTB.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            SeachTB.Size = new Size(200, 36);
+            SeachTB.TabIndex = 5;
             // 
             // TableForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(831, 817);
+            Controls.Add(SeachTB);
+            Controls.Add(label1);
+            Controls.Add(sideWayTable2);
+            Controls.Add(smallTable4);
+            Controls.Add(smallTable5);
+            Controls.Add(smallTable3);
+            Controls.Add(smallTable2);
+            Controls.Add(smallTable1);
+            Controls.Add(sideWayTable1);
+            Controls.Add(longTable2);
+            Controls.Add(longTable1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "TableForm";
             Text = "TableForm";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private LongTable longTable1;
+        private LongTable longTable2;
+        private SideWayTable sideWayTable1;
+        private SmallTable smallTable1;
+        private SmallTable smallTable2;
+        private SmallTable smallTable3;
+        private SmallTable smallTable4;
+        private SideWayTable sideWayTable2;
+        private SmallTable smallTable5;
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox SeachTB;
     }
 }

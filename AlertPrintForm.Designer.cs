c59c9e480114f6f2b627981795dd66c125540265
MinhThang <dominhtoan9467@gmail.com>
2025-07-13 14:14:01 +0700

    Fix giật lag khi load alert Print

--- AlertPrintForm.Designer.cs
+++ AlertPrintForm.Designer.cs
@@ -29,27 +29,22 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AlertPrintForm));
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             panel1 = new Panel();
             NotPrintButton = new Guna.UI2.WinForms.Guna2Button();
             PrintButton = new Guna.UI2.WinForms.Guna2Button();
             label2 = new Label();
             guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(components);
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // guna2Elipse1
-            // 
-            guna2Elipse1.BorderRadius = 20;
-            guna2Elipse1.TargetControl = this;
             // 
             // panel1
             // 
@@ -67,7 +62,7 @@
             // NotPrintButton
             // 
             NotPrintButton.BorderRadius = 20;
-            NotPrintButton.CustomizableEdges = customizableEdges7;
+            NotPrintButton.CustomizableEdges = customizableEdges1;
             NotPrintButton.DisabledState.BorderColor = Color.DarkGray;
             NotPrintButton.DisabledState.CustomBorderColor = Color.DarkGray;
             NotPrintButton.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -77,7 +72,7 @@
             NotPrintButton.ForeColor = Color.White;
             NotPrintButton.Location = new Point(203, 250);
             NotPrintButton.Name = "NotPrintButton";
-            NotPrintButton.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            NotPrintButton.ShadowDecoration.CustomizableEdges = customizableEdges2;
             NotPrintButton.Size = new Size(115, 50);
             NotPrintButton.TabIndex = 25;
             NotPrintButton.Text = "No";
@@ -86,7 +81,7 @@
             // PrintButton
             // 
             PrintButton.BorderRadius = 20;
-            PrintButton.CustomizableEdges = customizableEdges9;
+            PrintButton.CustomizableEdges = customizableEdges3;
             PrintButton.DisabledState.BorderColor = Color.DarkGray;
             PrintButton.DisabledState.CustomBorderColor = Color.DarkGray;
             PrintButton.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -96,7 +91,7 @@
             PrintButton.ForeColor = Color.White;
             PrintButton.Location = new Point(39, 250);
             PrintButton.Name = "PrintButton";
-            PrintButton.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            PrintButton.ShadowDecoration.CustomizableEdges = customizableEdges4;
             PrintButton.Size = new Size(115, 50);
             PrintButton.TabIndex = 26;
             PrintButton.Text = "Yes";
@@ -116,16 +111,24 @@
             // guna2PictureBox1
             // 
             guna2PictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            guna2PictureBox1.CustomizableEdges = customizableEdges11;
+            guna2PictureBox1.CustomizableEdges = customizableEdges5;
             guna2PictureBox1.Image = (Image)resources.GetObject("guna2PictureBox1.Image");
             guna2PictureBox1.ImageRotate = 0F;
             guna2PictureBox1.Location = new Point(96, 26);
             guna2PictureBox1.Name = "guna2PictureBox1";
-            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges6;
             guna2PictureBox1.Size = new Size(156, 130);
             guna2PictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             guna2PictureBox1.TabIndex = 23;
             guna2PictureBox1.TabStop = false;
+            // 
+            // guna2BorderlessForm1
+            // 
+            guna2BorderlessForm1.AnimationType = Guna.UI2.WinForms.Guna2BorderlessForm.AnimateWindowType.AW_HOR_POSITIVE;
+            guna2BorderlessForm1.BorderRadius = 20;
+            guna2BorderlessForm1.ContainerControl = this;
+            guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            guna2BorderlessForm1.TransparentWhileDrag = true;
             // 
             // AlertPrintForm
             // 
@@ -145,12 +148,11 @@
         }
 
         #endregion
-
-        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
         private Panel panel1;
         private Guna.UI2.WinForms.Guna2Button NotPrintButton;
         private Guna.UI2.WinForms.Guna2Button PrintButton;
         private Label label2;
         private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
     }
 }
